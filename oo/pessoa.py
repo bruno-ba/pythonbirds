59f5f2ec7ce7bd0a8750844cf436bce38e1405c8
bruno-ba <btmsba@hotmail.com>
2021-04-14 19:02:45 -0300

    criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,15 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None):
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa('bruno torres')
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Bruno'
+    print(p.nome)
